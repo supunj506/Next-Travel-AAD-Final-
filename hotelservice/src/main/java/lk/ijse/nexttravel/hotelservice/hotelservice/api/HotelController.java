97008f83a504571cafd940141f0c963c9d521a1c
supunj506 <supunj506@gmail.com>
2023-10-18 15:28:33 +0530

    hotel Service HotelController created

---
+++ hotelservice/src/main/java/lk/ijse/nexttravel/hotelservice/hotelservice/api/HotelController.java
@@ -0,0 +1,19 @@
+/*
+ * Author  : supun@mj
+ * Project : Spring Boot Final Project
+ * Date    : 10/18/2023
+ * Time    : 3:27 PM
+ * For GDSE course of IJSE Institute.
+ */
+
+package lk.ijse.nexttravel.hotelservice.hotelservice.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hotel")
+@CrossOrigin("*")
+public class HotelController {
+}
