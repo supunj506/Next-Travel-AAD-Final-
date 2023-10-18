52a0f4f17b4c9fd32b9a9194e3ec7cfc08d94446
supunj506 <supunj506@gmail.com>
2023-10-18 15:08:04 +0530

    hotel Service HotelService interface created

---
+++ hotelservice/src/main/java/lk/ijse/nexttravel/hotelservice/hotelservice/service/HotelService.java
@@ -0,0 +1,12 @@
+/*
+ * Author  : supun@mj
+ * Project : Spring Boot Final Project
+ * Date    : 10/18/2023
+ * Time    : 3:07 PM
+ * For GDSE course of IJSE Institute.
+ */
+
+package lk.ijse.nexttravel.hotelservice.hotelservice.service;
+
+public interface HotelService {
+}
