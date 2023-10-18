5ad48a168017409837adb9f5af1cbaf9a271e6af
supunj506 <supunj506@gmail.com>
2023-10-18 16:49:16 +0530

    hotel Service Get mehtod Imlemented and Work fine

--- hotelservice/src/main/java/lk/ijse/nexttravel/hotelservice/hotelservice/api/HotelController.java
+++ hotelservice/src/main/java/lk/ijse/nexttravel/hotelservice/hotelservice/api/HotelController.java
@@ -12,6 +12,7 @@ import lk.ijse.nexttravel.hotelservice.hotelservice.dto.HotelDTO;
 import lk.ijse.nexttravel.hotelservice.hotelservice.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,6 +31,11 @@ public class HotelController {
     @PostMapping(consumes = "application/json",produces = "application/json")
     HotelDTO saveHotel(@RequestBody HotelDTO hotelDTO){
         return  hotelService.saveHotel(hotelDTO);
+    }
+
+    @GetMapping(value = "{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    HotelDTO getHotel(@PathVariable String id){
+        return hotelService.getSelectHotel(id);
     }
 
 
