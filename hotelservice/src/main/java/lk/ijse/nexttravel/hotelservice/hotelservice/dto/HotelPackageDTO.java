6fbd156099ab4755e2efd4f7ca15c2b2b9049b8b
supunj506 <supunj506@gmail.com>
2023-10-18 15:06:34 +0530

    hotel Service HotelPackageDTO created

---
+++ hotelservice/src/main/java/lk/ijse/nexttravel/hotelservice/hotelservice/dto/HotelPackageDTO.java
@@ -0,0 +1,32 @@
+/*
+ * Author  : supun@mj
+ * Project : Spring Boot Final Project
+ * Date    : 10/18/2023
+ * Time    : 3:04 PM
+ * For GDSE course of IJSE Institute.
+ */
+
+package lk.ijse.nexttravel.hotelservice.hotelservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class HotelPackageDTO implements Serializable {
+
+    String hp_id;
+    String hp_category;
+    double hp_price;
+    String h_id;
+
+    public HotelPackageDTO(String hp_category, double hp_price, String h_id) {
+        this.hp_category = hp_category;
+        this.hp_price = hp_price;
+        this.h_id = h_id;
+    }
+}
