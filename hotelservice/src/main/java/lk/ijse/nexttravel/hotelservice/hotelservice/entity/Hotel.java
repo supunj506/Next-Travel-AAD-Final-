674860423e40c683e571573edfcfb260c443ace9
supunj506 <supunj506@gmail.com>
2023-10-18 14:07:45 +0530

    hotel Service Hotel Entity created

---
+++ hotelservice/src/main/java/lk/ijse/nexttravel/hotelservice/hotelservice/entity/Hotel.java
@@ -0,0 +1,10 @@
+/*
+ * Author  : supun@mj
+ * Project : Spring Boot Final Project
+ * Date    : 10/18/2023 
+ * Time    : 1:56 PM
+ * For GDSE course of IJSE Institute.
+ */
+ 
+package lk.ijse.nexttravel.hotelservice.hotelservice.entity;public class Hotel {
+}
